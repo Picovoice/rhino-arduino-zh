255f4021d08b5fd8504e91f5d248a2e6de8937ac
Eric Mikulin <ErisMik@users.noreply.github.com>
2026-01-09 10:24:55 -0800

    v4.0 release (#1)

---
+++ examples/RhinoExample/params.h
@@ -0,0 +1,21 @@
+
+/*
+    Copyright 2020-2021 Picovoice Inc.
+
+    You may not use this file except in compliance with the license. A copy of the license is located in the "LICENSE"
+    file accompanying this source.
+
+    Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+    an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+    specific language governing permissions and limitations under the License.
+*/
+
+#ifndef PV_PARAMS_H
+#define PV_PARAMS_H
+
+#include <stdint.h>
+
+static const uint8_t CONTEXT_ARRAY[] = {
+};
+
+#endif // PV_PARAMS_H
